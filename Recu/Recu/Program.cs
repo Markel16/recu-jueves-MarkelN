7ec70d890cdc59f0ecbb507ea7993bf4502cc9b4
Markel Nuñez Virto <markel_euneiz@euneiz.com>
2023-12-20 09:34:35 +0100

    acotaciones error

--- Recu/Recu/Program.cs
+++ Recu/Recu/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//create list Numbers(listNumbers) 
+
+// ask user to introduce the numbers de Jose Ramon Ruiz Gil
+
+// ask user to introduce the numbers
+
+// show in console the list of written numb... de Jose Ramon Ruiz Gil
+
+// show in console the list of written numbers(listNumbers)
+
+//show in console the ordered list de Jose Ramon Ruiz Gil
+
+
+//show in console the ordered list
+
+
